1012df408488d77befc43d539458eb3e20268ac2
alanhg <qianghe421@163.com>
2019-06-19 11:46:08 +0800

    init test

---
+++ test/cn/alanhe/CopyBookMarkXTest.java
@@ -0,0 +1,12 @@
+package test.test.cn.alanhe;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CopyBookMarkXTest {
+
+    @Test
+    public void should_say_hello() {
+        Assert.assertTrue(1 == 1);
+    }
+}
